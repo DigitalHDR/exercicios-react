0a5b97802d004b2da82bed4b754eb7e82d791a5b
DigitalHDR <digitalextremeultra@gmail.com>
2021-03-23 16:37:05 -0300

    menbro

--- src/componentes/MultiElementos.jsx
+++ src/componentes/MultiElementos.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props =>
-    <div>
+// export default props =>
+//     <React.Fragment>
+//         <h1>Parte1</h1>
+//         <h1>Parte2</h1>
+//     </React.Fragment>
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => [
+    <React.Fragment>
         <h1>Parte1</h1>
-        <h1>Parte1</h1>
-    </div>
-    
+        <h2>Parte2</h2>
+    </React.Fragment>
+]
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Primeiro from './componentes/PrimeiroComponente copy'
-import {CompA, CompB} from './componentes/SegundoComponente'
-
+import MultiElementos from './componentes/MultiElementos'
+// import Primeiro from './componentes/PrimeiroComponente copy'
+// import {CompA, CompB} from './componentes/SegundoComponente'
 
 ReactDOM.render(
-    <div>
-        <Primeiro valor="Bom dia!"  acbd={123} />
+    <React.Fragment>
+        <MultiElementos />
+        {/* <Primeiro valor="Bom dia!"  acbd={123} />
         <CompA valor='oi' />
-        <CompB valor='oi098' />
-    </div>
+        <CompB valor='oi098' /> */}
+    </React.Fragment>
 , document.getElementById('root'))
